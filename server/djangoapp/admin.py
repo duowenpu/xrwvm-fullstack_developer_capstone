eae8abdf651347715bceffd5f1a2facc4e3816a5
puzeng <duowenpu@gmail.com>
2025-01-09 14:27:35 -0800

    Black

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -14,6 +14,7 @@
 
 from django.contrib import admin
 from .models import CarMake, CarModel
+
 # Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel)
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -6,20 +6,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="https://puzeng-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
+    "backend_url",
+    default="https://puzeng-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+)
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="https://sentianalyzer.1qgkzyivll5o.us-south.codeengine.appdomain.cloud/")
+    "sentiment_analyzer_url",
+    default="https://sentianalyzer.1qgkzyivll5o.us-south.codeengine.appdomain.cloud/",
+)
+
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     print(request_url)
@@ -27,15 +31,17 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as  e:
+    except Exception as e:
         # If any error occurs
         print(e)
         print("Network exception occurred")
+
+
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     print(request_url)
     try:
         # Call get method of requests library with URL and parameters
@@ -45,13 +51,16 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # def post_review(data_dict):
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
         print("Network exception occurred")
+
+
 # Add code for posting review
